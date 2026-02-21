2c5d38c250d34e5ddcced4d8a85cca697387e5d1
leonardotrapani <ciao@leotrapani.com>
2026-02-21 13:08:42 +0100

    initial commit

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@omniscient/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+});
